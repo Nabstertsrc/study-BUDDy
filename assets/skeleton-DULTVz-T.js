ff7688c777a5a9f522c741b5d05de261975e8c35
Nabstertsrc <109488422+Nabstertsrc@users.noreply.github.com>
2026-07-19 20:02:56 +0000

    Deploying to gh-pages from @ Nabstertsrc/study-BUDDy@6745abf184334795a58c6da943f87f071e627582 🚀

--- assets/skeleton-DULTVz-T.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./vendor-BrPaZEzy.js";import{c as o}from"./index-Cc42MkYY.js";function a({className:r,...e}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-primary/10",r),...e})}export{a as S};
