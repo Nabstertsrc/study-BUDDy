8b0ba5e952c992ba72a4ad31dffb90cb6d6c9fdf
Nabstertsrc <109488422+Nabstertsrc@users.noreply.github.com>
2026-09-10 10:25:26 +0000

    Deploying to gh-pages from @ Nabstertsrc/study-BUDDy@9dc6ea1a7faefd0c8646a126c2f76ae91a79c16d 🚀

--- assets/AdGate-BILRxCoW.js
+++
@@ -1,1 +0,0 @@
-import{R as i,j as n}from"./vendor-DlaqCsmb.js";function f({featureName:l="AI Feature",onProceed:e,trigger:a,adSlot:s="AUTO",skipDelay:p=5,children:c}){const o=t=>{t&&(t.preventDefault(),t.stopPropagation()),e&&e()},r=a?i.cloneElement(a,{onClick:t=>{o(t)}}):null;return n.jsx(n.Fragment,{children:r})}export{f as A};
