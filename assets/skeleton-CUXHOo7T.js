4b8c8542e28a5294fe2e78ee98a7df438d688d64
Nabstertsrc <109488422+Nabstertsrc@users.noreply.github.com>
2026-09-18 05:48:41 +0000

    Deploying to gh-pages from @ Nabstertsrc/study-BUDDy@cf51d0b4c7cb251e5e6e470276e80c62f1758419 🚀

---
+++ assets/skeleton-CUXHOo7T.js
@@ -0,0 +1,1 @@
+import{j as m}from"./vendor-B5KLmi5v.js";import{c as o}from"./index-3TS-UiPl.js";function a({className:r,...e}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-primary/10",r),...e})}export{a as S};
